7dfc2720c2160bc7bad1cd312577531296e4b5af
sudipdigitalbasis <sudipdigitalbasis@github.com>
2021-08-08 15:29:27 +1000

    Installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EpisodeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('article/{article:slug}', ArticleController::class);
+Route::get('episode/{episode:slug}', EpisodeController::class);
+
+require __DIR__ . '/auth.php';
